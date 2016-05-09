39400b255bf2b18b3fbf4f2e2dfd11ed6caab992
taschini <taschini@users.noreply.github.com>
2016-05-09 11:43:12 +0200

    Testing the license type and project name in the setup files. Add flake8 testing to the setup files. Save all the collected setup metadata in a pickle file.

--- tests/test_setup_files.py
+++ tests/test_setup_files.py
@@ -1,4 +1,26 @@
-def test_setup_file(subproject, monkeypatch):
+import pytest
+
+
+@pytest.yield_fixture(scope="module")
+def collect():
+    """Return a list that is used as an accumulator.
+
+    At the end the list is saved in a pickle file called 'setups.pkl'.
+
+    """
+    collection = []
+    try:
+        yield collection
+    finally:
+        try:
+            import cPickle as pickle
+        except ImportError:
+            import pickle
+        with open('setups.pkl', 'wb') as f:
+            pickle.dump(collection, f, -1)
+
+
+def test_setup_file(subproject, monkeypatch, collect):
     import os
     import six
     import setuptools
@@ -14,5 +36,19 @@ def test_setup_file(subproject, monkeypatch):
         os.path.abspath('setup.py'),
         "exec"), namespace)
 
-    # THe long_description field is unicode
+    collect.append(setup)
+
+    # The long_description field is unicode
     assert isinstance(setup['long_description'], six.text_type)
+
+    # The license is BSD
+    assert setup['license'] == 'BSD'
+
+    # The library name is the same as the project name
+    assert os.path.basename(os.getcwd()) == setup['name']
+
+
+def test_flake8_setup(subproject):
+    from flake8.main import check_file
+
+    assert check_file('setup.py') == 0
